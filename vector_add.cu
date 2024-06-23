8ec13378d8b34dad1c7f1eea4ea66d5c8ea113b7
icantkode <ashersiddique127@gmail.com>
2024-06-23 23:57:50 +0500

    new book: vect add

--- vector_add.cu
+++ vector_add.cu
@@ -22,20 +22,29 @@ int main(void) {
     int *a, *b, *c;
     int *d_a, *d_b, *d_c;
     int size = N * sizeof(int);
+
     a = (int *)malloc(size);
-    fill_array(a);
     b = (int *)malloc(size);
-    fill_array(b);
     c = (int *)malloc(size);
+    fill_array(a);
+    fill_array(b);
+
     cudaMalloc((void *)&d_a, N * sizeof(int));
     cudaMalloc((void *)&d_b, N *sizeof(int));
     cudaMalloc((void *)&d_c, N * sizeof(int));
+
     cudaMemcpy(d_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, N* sizeof(int), cudaMemcpyHostToDevice);
+
     device_add<<<1,1>>>(d_a,d_b,d_c);
+
     cudaMemcpy(c, d_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+
     print_output(a,b,c);
+
     free(a); free(b); free(c);
+
     cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
     return 0;
 }
